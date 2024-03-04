d708ce2dafcda4387a6ccb6777185bcac4c0bda7
Imanol Dominguez Sanchez <imanol.desarrolloweb@gmail.com>
2024-03-04 14:17:49 -0300

    initial commit

---
+++ src/ChallengeApp.tsx
@@ -0,0 +1,11 @@
+import 'react-native-gesture-handler';
+import {NavigationContainer} from '@react-navigation/native';
+import {View, Text} from 'react-native';
+
+export const ChallengeApp = () => {
+  return (
+    <NavigationContainer>
+      <Text></Text>
+    </NavigationContainer>
+  );
+};
