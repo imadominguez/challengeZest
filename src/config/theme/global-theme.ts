03042d719eab3a540d24d847dfc1f690b7facb47
Imanol Dominguez Sanchez <imanol.desarrolloweb@gmail.com>
2024-03-04 17:25:39 -0300

    feat: configuracion de estilos globales

---
+++ src/config/theme/global-theme.ts
@@ -0,0 +1,12 @@
+import {StyleSheet} from 'react-native';
+
+export const globalTheme = StyleSheet.create({
+  globalMargin: {
+    marginHorizontal: 20,
+  },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
