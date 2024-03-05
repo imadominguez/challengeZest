200696230a32cb6e8d35ed7171ba60a0344c8c98
Imanol Dominguez Sanchez <imanol.desarrolloweb@gmail.com>
2024-03-05 20:44:40 -0300

    feat: paleta de colores dark, light

--- src/config/theme/global-theme.ts
+++ src/config/theme/global-theme.ts
@@ -1,5 +1,30 @@
 import {StyleSheet} from 'react-native';
-import {useTheme} from 'react-native-paper';
+export const colors = {
+  dark: {
+    primary_100: '#FF5733',
+    primary_200: '#ff8a5f',
+    primary_300: '#fff3bf',
+    accent_100: '#FFC300',
+    accent_200: '#916600',
+    text_100: '#FFFFFF',
+    text_200: '#c0c0c0',
+    bg_100: '#1C1C1C',
+    bg_200: '#2b2b2b',
+    bg_300: '#434343',
+  },
+  light: {
+    primary_100: '#FF6600',
+    primary_200: '#ff983f',
+    primary_300: '#ffffa1',
+    accent_100: '#F5F5F5',
+    accent_200: '#929292',
+    text_100: '#1d1f21',
+    text_200: '#444648',
+    bg_100: '#ffffff',
+    bg_200: '#eaeaea',
+    bg_300: '#cccccc',
+  },
+};
 
 export const globalTheme = StyleSheet.create({
   globalMargin: {
