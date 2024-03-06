22909f486fa7ff0eb6fa0f6a21ccbf53223921f4
Imanol Dominguez Sanchez <imanol.desarrolloweb@gmail.com>
2024-03-06 14:45:58 -0300

    fix: tipado de respuesta de la api

--- src/actions/breweries/get-breweries.ts
+++ src/actions/breweries/get-breweries.ts
@@ -1,14 +1,15 @@
 import {breweryApi} from '../../config/api/BreweryApi';
 import {Brewerie} from '../../domain/entities/breweries';
+import {BreweryAPI} from '../../infrastructure/interfaces/brewerie-api.interface';
 import {BrewerieMapper} from '../../infrastructure/mappers/brewerie.mapper';
 
 export const getBreweries = async (
   page: number,
-  limit: number = 20,
+  limit?: number,
 ): Promise<Brewerie[]> => {
   try {
     const url = `/breweries?page=${page}&per_page=${limit}`;
-    const {data} = await breweryApi.get(url);
+    const {data} = await breweryApi.get<BreweryAPI[]>(url);
 
     const breweriesMapped: Brewerie[] = data.map(
       BrewerieMapper.brewerieApiToEntity,
